4e298e7fd764d0ad7b15d38d33c323a7f3ca9dfe
MesterBrok <Brokdeveloper@gmail.com>
2023-07-01 18:53:08 +0330

    Update User Controls

--- BrokYoutubeDownloader/MainWindow.xaml.cs
+++ BrokYoutubeDownloader/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using BrokYoutubeDownloader.UserControls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,11 +27,54 @@ namespace BrokYoutubeDownloader
         {
             InitializeComponent();
         }
-
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
                 DragMove();
         }
+        public object privateTag { get; set; }
+        private void usItem_Path_Click(object sender, EventArgs e)
+        {
+            var userControlToDelete = UsItem(privateTag);
+            if (userControlToDelete != null)
+            {
+                spItems.Children.Remove(userControlToDelete);
+            }
+        }
+
+        private void ccButton_Click(object sender, RoutedEventArgs e)
+        {
+            var item = new UserControls.usItem()
+            {
+                Name = spItems.Name + spItems.Children.Count,
+                MyTag = spItems.Children.Count,
+            };
+            item.Path_Click += usItem_Path_Click;
+            item.MouseEnter += Item_MouseEnter;
+            item.ccLabel1.Title = spItems.Children.Count + "";
+            spItems.Children.Add(item);
+
+        }
+
+        private void Item_MouseEnter(object sender, MouseEventArgs e)
+        {
+            privateTag = (sender as UserControls.usItem).MyTag;
+            //MessageBox.Show(tag + "");
+        }
+
+        public UserControls.usItem UsItem(object tag)
+        {
+            foreach (var item in spItems.Children)
+            {
+                if (item is usItem us && us.MyTag == tag)
+                {
+                    return us;
+                }
+            }
+            return null;
+        }
+
+
+
     }
 }
--- BrokYoutubeDownloader/UserControls/usItem.xaml.cs
+++ BrokYoutubeDownloader/UserControls/usItem.xaml.cs
@@ -24,5 +24,27 @@ namespace BrokYoutubeDownloader.UserControls
         {
             InitializeComponent();
         }
+
+        public event EventHandler<EventArgs> Path_Click;
+
+
+        public object MyTag
+        {
+            get { return (object)GetValue(MyTagProperty); }
+            set { SetValue(MyTagProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyTag.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyTagProperty =
+            DependencyProperty.Register("MyTag", typeof(object), typeof(usItem), new PropertyMetadata(default));
+
+
+        private void Path_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Path_Click != null)
+            {
+                Path_Click(sender, e);
+            }
+        }
     }
 }
