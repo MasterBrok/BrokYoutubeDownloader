5657e68ee4f2161e3895e3eb450a40097aaa62b1
MesterBrok <Brokdeveloper@gmail.com>
2023-07-03 08:31:09 +0330

    add class validate for Url Link

---
+++ BrokYoutubeDownloader/Validates/ValidateUrl.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BrokYoutubeDownloader.Validates
+{
+    public class ValidateUrl
+    {
+        public static bool IsYouTubeLinkValid(string link)
+        {
+            string youtubePattern = @"^(https?://)?(www\.)?(youtube\.com|youtu\.be)/.+";
+
+            Regex regex = new Regex(youtubePattern);
+            return regex.IsMatch(link);
+        }
+    }
+}
